21ad5c183c3e5eb8771b96387370c78540efe6d1
sturple <shawn.turple@shaw.ca>
2015-12-28 12:47:59 -0800

    All features have been added. DefaultController * Operational CSV File * Operation Emailer * Added documentation * organized the structure
    results.html.twig
    * updated to allow for yaml file configuration to show correct dropdown
    menus
    
    email-base.txt.twig
    * added file to add text based emails.

--- Controller/DefaultController.php
+++ Controller/DefaultController.php
@@ -18,92 +18,71 @@ class DefaultController extends Controller
 	var $config = array();
 	var $logger = false;
 	
+	/**
+	 * route to show home page
+	 *
+	 */
     public function indexAction()
     {
         return $this->render('FgmsSurveyBundle:Default:index.html.twig', array());
     }
     
+	/**
+	 * route to start survey
+	 * @param string $slug
+	 * @param mixed $group
+	 *
+	 */
     public function startAction($slug,$group=false)
     {
 		$param = $this->getConfiguration($slug,$group);       	
         return $this->render('FgmsSurveyBundle:Default:start.html.twig', $param);   
     }
 
+    /**
+	 * route for the survey
+	 * @param string $slug
+	 * @param mixed $group
+	 *
+	 */
+    public function surveyAction($slug,$group=false)
+    {
+        $room = $this->get('request')->query->has('room') ? $this->get('request')->query->get('room') : 'none';
+        $param = $this->getConfiguration($slug,$group);		
+        $form = $this->getForm($slug,$group,$room);        
+		$form->handleRequest($this->get('request'));
+		if ($form->isValid()){			
+			$em = $this->getDoctrine()->getManager();
+			$em->persist($form->getData());
+			$this->sendEmails($slug,$group,$form,$param,$room);
+			$em->flush();          
+			return $this->redirect("/". $this->fullSlug. "/finish/");
+		}        
+        $param['form'] = $form->createView();       
+        return $this->render('FgmsSurveyBundle:Default:survey.html.twig', $param );
+    }	
+	
+	/**
+	 * route to finish survey
+	 * @param string $slug
+	 * @param mixed $group
+	 *
+	 */
     public function finishAction($slug,$group=false)
     {
 		$param = $this->getConfiguration($slug, $group);        
 		return $this->render('FgmsSurveyBundle:Default:finish.html.twig', $param);   
     }
-    
-    public function downloadcsvAction($slug,$group=false)
-    {
-		$param = $this->getConfiguration($slug, $group); 
-        $em = $this->getDoctrine()->getManager();
-        $sql = "SELECT
-				s.createDate,
-				s.slug,
-				s.sluggroup
-		";
- 
- 
-        $sql .= "FROM questionnaire s WHERE s.slug = '{$slug}'";
-		if ($group != false){
-			$sql .= " AND s.sluggroup = '{$group}'";
-		}
-		$this->logger->error($sql);
-        $rows =  $em->getConnection()->fetchAssoc($sql);
-		$this->logger->error('results:: '.print_R($rows));
-       // $repository = $this->getDoctrine()->getRepository('FgmsSurveyBundle:Questionnaire'); 
-       // $data = $repository->findAll();		
-        
-		$response = new StreamedResponse();		
-	
-		$response->setCallback(function(){
-			$handle = fopen('php://output', 'w+');
-			fputcsv($handle, array('Email', 'Last Name'),',');			
-           // fclose($handle);
-        });
-        
-        
-        //$response = $this->render('FgmsSurveyBundle:Default:csv.html.twig',array('data'=>$data));  
-		//$this->logger->notice('ORDERS '.print_R($orders,true));
-		$response->setStatusCode(200);
-		$response->headers->set('Content-Type', 'application/force-download');
-		$response->headers->set('Content-Type', 'text/plain; charset=utf-8');
-		;
-		$response->headers->set('Content-Disposition', $response->headers->makeDisposition( ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'export.txt'));
-		//$response->headers->set('Content-Disposition','attachment; filename=export.txt');
-		$this->get('logger')->error(print_r($response->getStatusCode(),true));
-		$response->prepare($this->get('request'));
-      
-       // $response->headers->set('Content-Disposition', 'attachment; filename="export.csv"');        
-        
-/*
-		$response = new StreamedResponse();	
-		$response->setCallback(function(){
-			$handle = fopen('php://output', 'w+');
-			fputcsv($handle, array('Email', 'Last Name','First Name', 'Date','Total Spent'),',');			
 
-			foreach ($data as $item){					
-				fputcsv($handle, array($item->question1));					
-				
-			}
-            fclose($handle);
-			
-		});	
-		
-		$response->headers->set('Content-Type', 'application/force-download');
-		$response->headers->set('Content-Disposition','attachment; filename="export.csv"');*/
-        
-        return $response;
-    
-    
-    
-    }
-    
-    
-    public function resultsAction($slug,$group=false)     {
-      
+    /**
+	 * route to get results this is the admin section requires valid key which is setup in yaml file
+	 *
+	 * @param string $slug
+	 * @param mixed $group
+	 *
+	 */
+    public function resultsAction($slug,$group=false)
+	{      
 		$param = $this->getConfiguration($slug,$group);
 		$showResultsFlag = false;
 		$key = $this->get('request')->query->has('key') ? $this->get('request')->query->get('key') : '--no key--';
@@ -132,8 +111,7 @@ class DefaultController extends Controller
         // getting each interval
         $survey7 = $this->getSurveyRollup($slug,$group,'7 DAY', $param['questions']);
         $survey30 = $this->getSurveyRollup($slug,$group,'30 DAY', $param['questions']);
-        $surveyYear = $this->getSurveyRollup($slug,$group,'1 YEAR', $param['questions']);
-              
+        $surveyYear = $this->getSurveyRollup($slug,$group,'1 YEAR', $param['questions']);              
         
         $stats = array();
         // setting up stats.
@@ -154,43 +132,91 @@ class DefaultController extends Controller
                              'yeartodate'=>$this->getValue($field,$surveyYear),
                              'trigger'=>$this->getValue('trigger',$allquestion),
 							 'emailtrigger'=>$emailtrigger
-                            );
-                             
+                            );                            
             
         }
         $param['stats'] = $stats;
         return $this->render('FgmsSurveyBundle:Default:results.html.twig',$param ); 
     }
     
-    
-    
-    public function surveyAction($slug,$group=false)
+	/**
+	 *
+	 * route to download CSV file
+	 * @param string $slug
+	 * @param mixed $group
+	 *
+	 */
+    public function downloadcsvAction($slug,$group=false)
     {
-        $room = $this->get('request')->query->has('room') ? $this->get('request')->query->get('room') : 'none';
-        $param = $this->getConfiguration($slug,$group);
-		
-        $form = $this->getForm($slug,$group,$room);        
-		$form->handleRequest($this->get('request'));
+		$param = $this->getConfiguration($slug, $group);		
+        $repository = $this->getDoctrine()->getRepository('FgmsSurveyBundle:Questionnaire');
+		$sluggroup = ($group != false) ? $group : '';
+		$query = $repository->createQueryBuilder('q')		
+			->where('q.slug = :slug AND q.sluggroup = :sluggroup')
+			->setParameters(array('slug' =>$slug, 'sluggroup' =>$sluggroup))
+			->orderBy('q.createDate','ASC')
+			->getQuery();		
+		$this->logger->error('SQL: ' . $query->getSQL());
+		$response = new StreamedResponse();		
+	
+		$response->setCallback(function() use ($query){				
+			$handle = fopen('php://output', 'w+');
+			fputcsv($handle, array('Date', 'Time','Group','Property','Room','Q1','Q2','Q3','Q4','Q5','Q6','Q7','Q8','Q9','Q10','Comment','id'),',');			
+			foreach ($query->getResult() as $item){					
+				fputcsv($handle, array($item->getCreateDate()->format('y-m-d'),
+									   $item->getCreateDate()->format('H:i'),
+									   $item->getSluggroup(),
+									   $item->getSlug(),
+									   $item->getRoomNumber(),
+									   $item->getQuestion1(),
+									   $item->getQuestion2(),
+									   $item->getQuestion3(),
+									   $item->getQuestion4(),
+									   $item->getQuestion5(),
+									   $item->getQuestion6(),
+									   $item->getQuestion7(),
+									   $item->getQuestion8(),
+									   $item->getQuestion9(),
+									   $item->getQuestion10(),
+									   $item->getQuestionComment(),
+									   $item->getId()						
+						));		
+			}
+            fclose($handle);
+        });
+		$response->setStatusCode(200);
+		$response->headers->set('Content-Type', 'application/force-download');
+		$response->headers->set('Content-Type', 'text/csv; charset=utf-8');
+		$response->headers->set('Content-Disposition', $response->headers->makeDisposition( ResponseHeaderBag::DISPOSITION_ATTACHMENT, 'export.csv'));
+		$this->get('logger')->error(print_r($response->getStatusCode(),true));
+		$response->prepare($this->get('request'));        
+        return $response;    
+    } 
 
-		if ($form->isValid()){			
-			$em = $this->getDoctrine()->getManager();
-			$em->persist($form->getData());
-			$this->sendEmails($slug,$group,$form,$param,$room);
-			$em->flush();
-          
-			return $this->redirect("/". $this->fullSlug. "/finish/");
-		}        
-        $param['form'] = $form->createView();       
-        return $this->render('FgmsSurveyBundle:Default:survey.html.twig', $param );
-    }
     
-	public function emailpreviewAction($slug,$group=false){
+	/**
+	 * route to preview email
+	 * @param string $slug
+	 * @param mixed $group
+	 */
+	public function emailpreviewAction($slug,$group=false)
+	{
 		$param = $this->getConfiguration($slug,$group);
 		$this->sendEmails($slug, $group,false,$param,'testroom');
 		return $this->render('FgmsSurveyBundle:Email:email-base.html.twig',$param ); 
-	}
-    
-    private function sendEmails($slug, $group, $form,$param,$room){
+	}    
+	
+	/**
+	 *
+	 * @param string $slug
+	 * @param string $group
+	 * @param form $form
+	 * @param array $param
+	 * @param string $room
+	 *
+	 */
+    private function sendEmails($slug, $group, $form,$param,$room)
+	{
         $l = $this->get('logger');
 		$param = $this->getConfiguration($slug,$group);
         foreach ($param['questions'] as $item){
@@ -203,8 +229,10 @@ class DefaultController extends Controller
 			else {
 				$data = $form->get($field)->getData();    
 			}
-			
-            $trigger = isset($item['email']['trigger']) ? $item['email']['trigger'] : false;
+			if ($this->getValue('email',$item,false) == false){
+				continue;
+			}
+            $trigger = $this->getValue('trigger',$item['email']);
 			$triggerType = intval($data);
 			//comparison type
 			if (($triggerType > 0) && ($trigger != null)){				
@@ -217,36 +245,49 @@ class DefaultController extends Controller
 				$emailFlag = true;
 			}
             
-			
+			// this means that an email trigger has been tripped need to send email.
             if ($emailFlag){
 				$item['answer'] = $data;
 				$item['roomNumber'] = $room;
-				$emailSubject = sprintf($item['email']['subject'],  str_replace('question','Q',$item['field']), $room);          
+				$item['title'] = strip_tags($item['title']);
+				$item['email']['recipient']['bcc'] = array_merge($item['email']['recipient']['bcc'],array('webmaster@fifthgeardev.com'));
+				
+				$emailSubject = '** '. $param['property']['name'] . ' - '. sprintf($item['email']['subject'],  str_replace('question','Q',$item['field']), $room);          
 				$l->error('trigger '.$trigger. ' value ' . $data . $emailSubject);
 				$combined_array = array_merge($param, array('item'=>$item));					
 				$message = \Swift_Message::newInstance()
                     ->setSubject($emailSubject)
-                    ->setFrom('postmaster@fifthgeardev.net')
-                    ->setTo('shawn@turple.ca')
-                    ->setBody($this->renderView('FgmsSurveyBundle:Email:email-base.html.twig', $combined_array ),'text/html');
-                //$this->get('mailer')->send($message);	  
-				
-				
-            }            
-            
-        }        
-        
-      
+                    ->setFrom('postmaster@guestfeedback.net');
+				// setting up recipients
+				if ($this->getValue('recipient',$item['email']) != false){
+					if ($this->getValue('to',$item['email']['recipient']) != false){
+						$message  = $message->setTo($item['email']['recipient']['to']);						
+					}
+					if ($this->getValue('cc',$item['email']['recipient']) != false){						
+						$message  = $message->setCc($item['email']['recipient']['cc']);
+						
+					}
+					if ($this->getValue('bcc',$item['email']['recipient']) != false){						
+						$message  = $message->setBcc($item['email']['recipient']['bcc']);						
+					}					
+				}
+				$message = $message                   
+                    ->setBody($this->renderView('FgmsSurveyBundle:Email:email-base.html.twig', $combined_array ),'text/html')
+					->addPart($this->renderView('FgmsSurveyBundle:Email:email-base.txt.twig', $combined_array ),'text/plain');
+                $this->get('mailer')->send($message);	
+            }  
+        } 
     }
-    
+	
 	/*
 	 * gets configurations for property and sets the images path 
 	 *
-	 *@param string $slug this is the slug or permalink of property
-	 *@param string/boolean $groupslug if no option passed just uses slug otherwise uses it as a folder name
+	 * @param string $slug this is the slug or permalink of property
+	 * @param string/boolean $groupslug if no option passed just uses slug otherwise uses it as a folder name
 	 *
 	 */
-	private function getConfiguration($slug, $group=false){
+	private function getConfiguration($slug, $group=false)
+	{
 		if ($this->logger === false){
 			$this->logger = $this->get('logger');
 		}
@@ -272,8 +313,7 @@ class DefaultController extends Controller
 		return array('error'=>$error_str );
 		}
 
-		$property_file = $this->config['property_config_dir'] .$this->fullSlug.".yml";
-		
+		$property_file = $this->config['property_config_dir'] .$this->fullSlug.".yml";		
 		if (!file_exists($property_file)){
 			$error_str = 'Property Config file "' . $property_file .'" does not exists';
 			throw $this->createNotFoundException($error_str);
@@ -286,19 +326,16 @@ class DefaultController extends Controller
 		}
 		$this->logger->error('CONFIG File '.$property_file);
 		$param['fullslug'] = $this->fullSlug;
-		return array_merge($param,$this->config);
-
-	
+		$param['key'] = $this->get('request')->query->has('key') ? $this->get('request')->query->get('key') : '--no key--';
+		return array_merge($param,$this->config);	
 	}
 	
-
 	/*
 	 * Gets Survey Rollup with specific interval
 	 *
 	 * @param string $property this is property slug
 	 * @param string $timeInterval is the time interval for the last x time
 	 * @param array $allquestions is an array of all the questions
-	 *
 	 */
     private function getSurveyRollup($slug, $group, $timeInterval="7 DAY", $allquestions)
     {        
@@ -329,19 +366,16 @@ class DefaultController extends Controller
         return $em->getConnection()->fetchAssoc($sql);
     }
      
-    
-    /*
-	 *
+    /**
 	 * Gets the form to display for survey
 	 *
 	 * @param string $property is the property slug
 	 * @param string $roomNumber is the room number
-	 * @param array $questions this is an array of all the questions 
-	 *
-	 *
+	 * @param array $questions this is an array of all the questions
+	 * @return form
 	 */
-    private function getForm($slug,$group, $roomNumber='none'){      
-        
+    private function getForm($slug,$group, $roomNumber='none')
+	{         
         $questionObject = new Questionnaire();
         $questionObject->setCreateDate();
         $questionObject->setSlug($slug);
@@ -349,12 +383,7 @@ class DefaultController extends Controller
 		if ($questionObject->getRoomNumber() == null){   
             $questionObject->setRoomNumber($roomNumber);
 		}
-		/*
-        if ($questionObject->getQuestionSet() == null){
-            $questionObject->setQuestionSet(strip_tags(json_encode($questions)));
-        }
-        */
-		
+	
 		$form = $this->createFormBuilder($questionObject, array('csrf_protection' => true))
 			->add('question1','hidden')
 			->add('question2','hidden')
@@ -371,7 +400,15 @@ class DefaultController extends Controller
 		return $form;		
 	}
 	
-	private function getValue($key, $array=array(), $default=false){
+	/**
+	 *
+	 * @param string $key
+	 * @param array $array
+	 * @param mixed $default
+	 * @return mixed 
+	 */
+	private function getValue($key, $array=array(), $default=false)
+	{
 		if (isset($array[$key])){
 			return $array[$key];
 		}

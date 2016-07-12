36b40474d71670eb71af550549c36f311b9fab2c
Robert Allan Hennigan Leahy <rleahy@rleahy.ca>
2016-07-11 22:15:14 -0700

    Performance Charting Front-End - Refactor
    Makes code more manageable by separated the column and pie chart code.  This allows them to be collapsed separately in an IDE and (more importantly) gives them their own scopes.

--- Resources/public/js/admin/performancecharting.js
+++ Resources/public/js/admin/performancecharting.js
@@ -40,122 +40,128 @@ define(['jquery','google/visualization','moment-timezone'],function ($, visualiz
 					return;
 				}
 				if (!enabled) return;
-				var table = new visualization.DataTable();
-				table.addColumn('date','Date');
-				table.addColumn('number','Q' + question);
-				table.addColumn({
-					role: 'style',
-					type: 'string'
-				});
-				var begin = null;
-				var end = null;
-				var has_data = false;
-				data.results.forEach(function (result) {
-					var m = moment.unix(result.begin).tz(data.timezone);
-					var day = new Date(m.year(),m.month(),m.date());
-					if (begin === null) begin = day;
-					end = day;
-					if (result.value !== null) has_data=true;
-					var color = '#dfc12a';	//	Mustard yellow
-					if (result.value === data.max) color = '#228b22';	//	Forest green
-					else if ((data.threshold !== null) && (result.value < data.threshold)) color = 'red';
-					table.addRow([day,result.value,'color: ' + color]);
-				});
-				var title = null;
-				if (data.type === 'open') {
-					title = '% Responding';
-				} else if (data.type === 'polar') {
-					title = '% Positive';
-				} else {
-					//	Must be rating
-					title = 'Average Rating';
-				}
-				var config={
-					title: data.title + '\nDaily Averages',
-					hAxis: {title: 'Date'},
-					vAxis: {
-						title: title,
-						viewWindow: {
-							min: (data.min === 0) ? -10 : 0,
-							max: (data.max === 100) ? 110 : 6
+				//	Column chart
+				(function () {
+					var table = new visualization.DataTable();
+					table.addColumn('date','Date');
+					table.addColumn('number','Q' + question);
+					table.addColumn({
+						role: 'style',
+						type: 'string'
+					});
+					var begin = null;
+					var end = null;
+					var has_data = false;
+					data.results.forEach(function (result) {
+						var m = moment.unix(result.begin).tz(data.timezone);
+						var day = new Date(m.year(),m.month(),m.date());
+						if (begin === null) begin = day;
+						end = day;
+						if (result.value !== null) has_data=true;
+						var color = '#dfc12a';	//	Mustard yellow
+						if (result.value === data.max) color = '#228b22';	//	Forest green
+						else if ((data.threshold !== null) && (result.value < data.threshold)) color = 'red';
+						table.addRow([day,result.value,'color: ' + color]);
+					});
+					var title = null;
+					if (data.type === 'open') {
+						title = '% Responding';
+					} else if (data.type === 'polar') {
+						title = '% Positive';
+					} else {
+						//	Must be rating
+						title = 'Average Rating';
+					}
+					var config={
+						title: data.title + '\nDaily Averages',
+						hAxis: {title: 'Date'},
+						vAxis: {
+							title: title,
+							viewWindow: {
+								min: (data.min === 0) ? -10 : 0,
+								max: (data.max === 100) ? 110 : 6
+							},
+							ticks: (data.max === 100) ? [0,25,50,75,100] : [1,2,3,4,5]
 						},
-						ticks: (data.max === 100) ? [0,25,50,75,100] : [1,2,3,4,5]
-					},
-					legend: {
-						position: 'none'
-					}
-				};
-				if (begin !== null) {
-					config.hAxis.viewWindow = {
-						max: end,
-						min: begin
+						legend: {
+							position: 'none'
+						}
 					};
-				}
-				if (data.threshold !== null) {
-					config.title += '\nThreshold: ' + data.threshold;
-					if (data.max === 100) config.title += '%';
-				}
-				if (!has_data) {
-					config.title += '\nNO DATA';
-				}
-				chart.draw(table,config);
-				image.attr('href',chart.getImageURI());
-				get_bar_image_filename(question,days,function (filename, e) {
-					if (e) {
-						report_error(e);
-						return;
+					if (begin !== null) {
+						config.hAxis.viewWindow = {
+							max: end,
+							min: begin
+						};
 					}
-					image.attr('download',filename);
-				});
-				var pie_table = new visualization.DataTable();
-				var pie_options = {
-					title: data.title + '\nSummary'
-				};
-				//	Branch on type of question because we do three
-				//	different things for the pie chart for them
-				if (data.type === 'rating') {
-					pie_table.addColumn('string','Rating');
-					pie_table.addColumn('number','Entries');
-					pie_options.slices = {};
-					var r = 255;
-					var r_step = -(255-0x22) / 4;
-					var g = 0;
-					var g_step = 0x8b / 4;
-					var b = 0;
-					var b_step = 0x22 / 4;
-					for (var i = 0;i < 5;++i) {
-						pie_options.slices[i] = {color: 'rgb(' + r.toFixed() + ',' + g.toFixed() + ',' + b.toFixed() + ')'};
-						r += r_step;
-						g += g_step;
-						b += b_step;
-						var arr = [(i+1).toString(),data.summary.values[i]];
-						console.log(arr);
-						pie_table.addRow(arr);
+					if (data.threshold !== null) {
+						config.title += '\nThreshold: ' + data.threshold;
+						if (data.max === 100) config.title += '%';
 					}
-				} else if (data.type === 'polar') {
-					pie_options.slices = {
-						0: {color: '#228b22'},
-						1: {color: 'red'}
+					if (!has_data) {
+						config.title += '\nNO DATA';
+					}
+					chart.draw(table,config);
+					image.attr('href',chart.getImageURI());
+					get_bar_image_filename(question,days,function (filename, e) {
+						if (e) {
+							report_error(e);
+							return;
+						}
+						image.attr('download',filename);
+					});
+				})();
+				//	Pie chart
+				(function () {
+					var pie_table = new visualization.DataTable();
+					var pie_options = {
+						title: data.title + '\nSummary'
 					};
-					pie_table.addColumn('string','Answer Category');
-					pie_table.addColumn('number','Entries');
-					pie_table.addRow(['Positive',data.summary.good]);
-					pie_table.addRow(['Negative',data.summary.bad]);
-				} else {
-					pie_table.addColumn('string','Answer Category');
-					pie_table.addColumn('number','Entries');
-					pie_table.addRow(['Answered',data.summary.good]);
-					pie_table.addRow(['Unanswered',data.summary.bad]);
-				}
-				pie_chart.draw(pie_table,pie_options);
-				pie_image.attr('href',pie_chart.getImageURI());
-				get_pie_image_filename(question,days,function (filename, e) {
-					if (e) {
-						report_error(e);
-						return;
+					//	Branch on type of question because we do three
+					//	different things for the pie chart for them
+					if (data.type === 'rating') {
+						pie_table.addColumn('string','Rating');
+						pie_table.addColumn('number','Entries');
+						pie_options.slices = {};
+						var r = 255;
+						var r_step = -(255-0x22) / 4;
+						var g = 0;
+						var g_step = 0x8b / 4;
+						var b = 0;
+						var b_step = 0x22 / 4;
+						for (var i = 0;i < 5;++i) {
+							pie_options.slices[i] = {color: 'rgb(' + r.toFixed() + ',' + g.toFixed() + ',' + b.toFixed() + ')'};
+							r += r_step;
+							g += g_step;
+							b += b_step;
+							var arr = [(i+1).toString(),data.summary.values[i]];
+							console.log(arr);
+							pie_table.addRow(arr);
+						}
+					} else if (data.type === 'polar') {
+						pie_options.slices = {
+							0: {color: '#228b22'},
+							1: {color: 'red'}
+						};
+						pie_table.addColumn('string','Answer Category');
+						pie_table.addColumn('number','Entries');
+						pie_table.addRow(['Positive',data.summary.good]);
+						pie_table.addRow(['Negative',data.summary.bad]);
+					} else {
+						pie_table.addColumn('string','Answer Category');
+						pie_table.addColumn('number','Entries');
+						pie_table.addRow(['Answered',data.summary.good]);
+						pie_table.addRow(['Unanswered',data.summary.bad]);
 					}
-					pie_image.attr('download',filename);
-				});
+					pie_chart.draw(pie_table,pie_options);
+					pie_image.attr('href',pie_chart.getImageURI());
+					get_pie_image_filename(question,days,function (filename, e) {
+						if (e) {
+							report_error(e);
+							return;
+						}
+						pie_image.attr('download',filename);
+					});
+				})();
 				last_days = days;
 				last_question = question;
 			});
